40d9b3d74dea859c2b3617042fc329193495bcf4
Mohamed-Maghraby <mohamedahmedseifofficial@gmail.com>
2022-11-09 07:13:55 +0300

    third

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 import pytesseract
 from PIL import Image
-
-
-x = 5
-print(x+9)
+import cv2
 
 pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+imgPath = 'folded-image.jpg'
 
-print(pytesseract.image_to_string('image.jpg'))
-myImage = Image.open('image.jpg')
-myImage.show()  
+img = cv2.imread(imgPath)
+img = cv2.resize(img, None, fx=0.5,fy=0.5)
+grayImage = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+text = pytesseract.image_to_string(grayImage)
+print(text)
+
+cv2.imshow('grayImage',grayImage)
+cv2.imshow('img',img)
+cv2.waitKey(0)
+
+# myImage = Image
+# 
+# 
+# 
+#.open(imgPath)
+# myImage.show() 
+
 
 
 
